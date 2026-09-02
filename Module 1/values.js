eb2fc26e6c3349c960feb577024c4152238f1e83
arefin-ahmed <arefin-ahmed@users.noreply.github.com>
2026-09-03 01:37:47 +0600

    falsy value

---
+++ Module 1/values.js
@@ -0,0 +1,22 @@
+// falsy value -  false, 0, "", null, undefined, NaN
+
+let age1 = 11;
+let age2 = " ";
+let age3 = 0;
+let age4 = "null";
+
+if (age1){
+    console.log("Age exists");
+}
+
+if (age2){
+    console.log("Not exists");
+}
+
+if (age3){
+    console.log("Not exists");
+}
+
+if (age4){
+    console.log("Not exists");
+}
