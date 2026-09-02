79da70f8ac911e1ce4e71501e44654c1a3a77029
arefin-ahmed <arefin-ahmed@users.noreply.github.com>
2026-09-02 13:33:10 +0600

    variable type print string number boolean

---
+++ Module 1/variable3.js
@@ -0,0 +1,11 @@
+let name = "Rahim";
+name = "Karim";
+// console.log(name);
+
+const age = 25;
+age = 30;
+// console.log(age);
+
+const isAdult = true;
+
+console.log(typeof name, typeof age, typeof isAdult); 
