eaded019eb12525bf06f0e4adb0b95bcc76e9a70
arefin-ahmed <arefin-ahmed@users.noreply.github.com>
2026-09-02 21:18:50 +0600

    string concatenation, escape character

---
+++ Module 1/string.js
@@ -0,0 +1,23 @@
+let firstname = "Arefin";
+let lastname = "Ahmed";
+// 1. Concatenation
+let fullname = firstname + " " + lastname;
+console.log(fullname);
+
+let fullname2 = `${firstname} ${lastname}`;
+console.log(fullname2);
+
+let fullname3 = `${firstname} ${lastname} is a good boy`;
+console.log(fullname3);
+
+let fullname4 = `${firstname} ${lastname} \nBangladesh`;
+console.log(fullname4);
+
+// 2. Escape character
+let random = "I am a \"student\"";
+console.log(random);
+
+// or
+
+let random2 = 'I am a "student"';
+console.log(random2);
