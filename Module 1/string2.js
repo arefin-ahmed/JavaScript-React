22cff898ae9946dccb391b525a053fad1addc2b8
arefin-ahmed <arefin-ahmed@users.noreply.github.com>
2026-09-02 21:52:35 +0600

    string upper, lower case convert, remove whitespace, length of the string

---
+++ Module 1/string2.js
@@ -0,0 +1,13 @@
+let address = "dhaka";
+console.log(address.toUpperCase());  // convert to upper case
+
+let address2 = "CHITTAGONG";
+console.log(address2.toLowerCase());  // convert to lower case
+
+let address3 = "    Khulna    ";
+console.log(address3.trim());  // remove whitespace from both ends of a string
+
+let address4 = "rajshahi is famous for mangoes";
+console.log(address4.includes("mangoes")); // check if the string contains "mangoes"
+
+console.log(address4.length); // get the length of the string
