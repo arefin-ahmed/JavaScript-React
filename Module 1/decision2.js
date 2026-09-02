725d3b985fafe0e7ad31478a712464fb73280970
arefin-ahmed <arefin-ahmed@users.noreply.github.com>
2026-09-02 22:43:17 +0600

    if, else-if, else

---
+++ Module 1/decision2.js
@@ -0,0 +1,17 @@
+let marks = 80;
+
+if (marks >= 95){
+    console.log("A+");
+}
+else if (marks >= 90){
+    console.log("A");
+}
+else if (marks >= 85){
+    console.log("A-");
+}
+else if (marks >= 80){
+    console.log("B+");
+}
+else {
+    console.log("Inprovement needed");
+}
